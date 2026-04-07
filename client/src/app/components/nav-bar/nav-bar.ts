87f898e75b21f5527a32562454062849a4971eb8
annkitabarman <ankitabr288@gmail.com>
2026-04-08 00:10:47 +0530

    adding to repo

---
+++ client/src/app/components/nav-bar/nav-bar.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [],
+  templateUrl: './nav-bar.html',
+  styleUrl: './nav-bar.scss',
+})
+export class NavBar {
+  toggleMenu() {
+    console.log('Menu toggled');
+  }
+}
