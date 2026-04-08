fedb9488d779065bd959bb9a3061d3457998440e
annkitabarman <ankitabr288@gmail.com>
2026-04-08 19:35:24 +0530

    add responsiveness

--- client/src/app/components/nav-bar/nav-bar.ts
+++ client/src/app/components/nav-bar/nav-bar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,7 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.scss',
 })
 export class NavBar {
+  isFullScreenSearchVisible = signal(false);
   toggleMenu() {
     console.log('Menu toggled');
+  }
+
+  displayOptions() {
+    console.log('Options displayed');
+  }
+
+  toggleSearchBox() {
+    this.isFullScreenSearchVisible.set(!this.isFullScreenSearchVisible());
+    console.log('Search box toggled');
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    if (window.innerWidth >= 640) {
+      this.isFullScreenSearchVisible.set(false);
+    }
   }
 }
