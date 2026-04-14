d29ee2f496fcadaee266ef59a03ada762750d7d8
annkitabarman <ankitabr288@gmail.com>
2026-04-14 20:20:08 +0530

    fetch trending movies

---
+++ client/src/app/services/movies-service.ts
@@ -0,0 +1,33 @@
+import { Injectable, inject } from '@angular/core';
+import { BASE_URL } from '../constants/api-urls';
+import { Observable, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import {
+  TrendingMoviesType,
+  TrendingMoviesAPIResponse,
+} from '../models/movie-response.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MoviesService {
+  private readonly _baseUrl = BASE_URL;
+
+  private readonly _httpClient = inject(HttpClient);
+
+  fetchTrendingMovies(): Observable<TrendingMoviesType[]> {
+    return this._httpClient
+      .get<TrendingMoviesAPIResponse>(`${this._baseUrl}/movie/popular`)
+      .pipe(
+        map((res) => res.results),
+        map((movies) => {
+          return movies.map((movie) => {
+            return {
+              ...movie,
+              poster_path: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            };
+          });
+        }),
+      );
+  }
+}
